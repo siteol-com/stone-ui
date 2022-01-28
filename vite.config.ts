500d483e7900c50d812b05eb32b37a111e1b27de
mebugs <iam@qiantaoyuan.cn>
2022-01-28 18:06:13 +0800

    Vue3.2+TS+AcroInit

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import eslint from 'vite-plugin-eslint';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    eslint({
+      cache: false,
+      include: ['src/**/*.ts', 'src/**/*.tsx', 'src/**/*.vue'],
+      exclude: ['node_modules'],
+    }),
+  ]
+})
